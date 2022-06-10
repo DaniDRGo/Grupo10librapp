e1ab26b1fd29a76f879c86792c363ed37c7869d2
Jorge <jorgecortesing@gmail.comgit config --global user.email jorgecortesing@gmail.comgit config --global user.email jorgecortesing@gmail.com>
2022-06-10 14:54:11 -0500

    Se hace el header responsive

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById("menu-hamburguesa");
+let navbar = document.getElementById("navbar");
+
+menu.addEventListener("click", function () {
+    console.log('click')
+    navbar.classList.toggle('menu-visible')
+});
