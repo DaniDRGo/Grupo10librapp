cb0b257d22eede6893a0893039e22ef39bc70874
Jorge <jorgecortesing@gmail.comgit config --global user.email jorgecortesing@gmail.comgit config --global user.email jorgecortesing@gmail.com>
2022-06-10 20:20:23 -0500

    Se da inicio a la pagina de home en mobile

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,37 @@ let menu = document.getElementById("menu-hamburguesa");
 let navbar = document.getElementById("navbar");
 
 menu.addEventListener("click", function () {
-    console.log('click')
-    navbar.classList.toggle('menu-visible')
+  console.log("click");
+  navbar.classList.toggle("menu-visible");
 });
+
+// Slider
+const slider = document.querySelector(".gallery");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", (_) => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", (_) => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 3;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+// Fin Slider
