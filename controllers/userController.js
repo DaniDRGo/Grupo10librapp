54e72e3056c62638b298f2e6da3092ab2e4439d1
Jorge <jorgecortesing@gmail.comgit config --global user.email jorgecortesing@gmail.comgit config --global user.email jorgecortesing@gmail.com>
2022-08-23 20:35:01 -0500

    Se traba el CRUD, especialmente en el Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,28 +6,32 @@ const db = require('../db/models');
 
 
 // LLamado a el archivo JSON
-const dbusers = path.join(__dirname, "../Database/dbusers.json");
-const users = JSON.parse(fs.readFileSync(dbusers, "utf-8"));
+// const dbusers = path.join(__dirname, "../Database/dbusers.json");
+// const users = JSON.parse(fs.readFileSync(dbusers, "utf-8"));
 
 const userController = {
   getAll: (req, res) => {
     db.Usuario.findAll()
-      .then(users => res.json( users ))
-    // res.render("users/users", { users });
+      .then(users => res.render("users/users", { users }))
   },
   getOne: (req, res) => {
-    let id = req.params.id;
-    let usuarioBuscado = users.find((user) => user.id == id);
-    res.render("users/userDetail", { usuarioBuscado });
+    db.Usuario.findByPk(req.params.id)
+      .then( user => { res.render("users/userDetail", { usuarioBuscado: user }) } )
   },
   showForm: (req, res) => {
     res.render("users/register");
   },
   createUser: (req, res) => {
+
+    let avatar = req.file.filename
+    let password = req.body.password
+    password = bcrypt.hashSync(password, 10);
+    let tyc = req.body.tc ? true : false;
+
     db.Usuario.create({
       nombre: req.body.nombre,
       apellido: req.body.apellido,
-      avatar: 'http://placeimg.com/480/640/any',
+      avatar,
       fecha_nacimiento: req.body.fecha,
       email: req.body.email,
       telefono: req.body.phone,
@@ -37,98 +41,37 @@ const userController = {
       direccion: req.body.direccion,
       piso: req.body.piso,
       cod_postal: req.body.cp,
-      password: req.body.password,
-      tyc: req.body.tc,
-      id_rol: 2 
-    })
-    // let idDefinition = users.length + 1;
-    // console.log(req.file);
-    // let {
-    //   nombre,
-    //   apellido,
-    //   avatar,
-    //   fecha,
-    //   email,
-    //   phone,
-    //   pais,
-    //   provincia,
-    //   localidad,
-    //   direccion,
-    //   piso,
-    //   cp,
-    //   password,
-    //   tc,
-    // } = req.body;
-
-    // password = bcrypt.hashSync(password, 10);
-
-    // let newUser = {
-    //   id: idDefinition,
-    //   nombre,
-    //   apellido,
-    //   avatar,
-    //   fecha,
-    //   email,
-    //   phone,
-    //   pais,
-    //   provincia,
-    //   localidad,
-    //   direccion,
-    //   piso,
-    //   cp,
-    //   password,
-    //   TYC: tc,
-    //   isAdmin: false,
-    // };
-
-    // //Asignación del nombre de la imagen para poder guardarla en BBDD
-    // newUser.avatar = req.file.filename;
-    // // Captura la inforacion edl radio button , para hacer validaciones posteriores
-    // newUser.TYC = newUser.TYC ? true : false;
-
-    // users.unshift(newUser);
-    // let usersReady = JSON.stringify(users);
-    // fs.writeFileSync("./Database/dbusers.json", usersReady);
-    // res.redirect("/users");
+      password,
+      tyc,
+      id_rol: 2 // este dato solo se cambia desde BBDD , hay que hacer una función para poderlo editar desde la app
+    });
+    res.redirect("/users");
   },
   showFormEdit: (req, res) => {
-    let userToEdit = users.find((user) => user.id == req.params.id);
-    res.render("users/editUser", { userToEdit });
+    db.Usuario.findByPk(req.params.id)
+      .then( user => res.render("users/editUser", { userToEdit: user }) )
+    // let userToEdit = users.find((user) => user.id == req.params.id);
+    // res.render("users/editUser", { userToEdit });
   },
   editUser: (req, res) => {
-    const idBuscado = req.params.id;
-
-    let userModification = ({
-      nombre,
-      apellido,
-      avatar,
-      fecha,
-      email,
-      phone,
-      pais,
-      provincia,
-      localidad,
-      direccion,
-      piso,
-      cp,
-      password,
-      isAdmin,
-    } = req.body);
-
-    let updatedUser = {
-      id: idBuscado,
-      ...userModification,
-    };
-
-    for (const i in users) {
-      if (users[i].id == idBuscado) {
-        users[i] = updatedUser;
-        break;
+        
+    db.Usuario.update({
+      nombre: req.body.nombre,
+      apellido: req.body.apellido,
+      fecha_nacimiento: req.body.fecha,
+      email: req.body.email,
+      telefono: req.body.phone,
+      pais: req.body.pais,
+      provincia: req.body.provincia,
+      localidad: req.body.localidad,
+      direccion: req.body.direccion,
+      piso: req.body.piso,
+      cod_postal: req.body.cp
+    },{
+      where: {
+        id_usuario: req.params.id
       }
-    }
-
-    fs.writeFileSync("./Database/dbusers.json", JSON.stringify(users), {encoding: "utf-8"});
-
+    });
     res.redirect("/users");
   },
   destroyUser: (req, res) => {
